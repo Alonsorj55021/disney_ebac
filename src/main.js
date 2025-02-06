a30910e40b953c555222613d0a0a81d3a4d76a54
Alonsorj55021 <rafaelpacheco.rj@gmail.com>
2025-02-06 16:02:24 -0300

    seçao shows

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
-    
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
@@ -10,7 +9,6 @@ document.addEventListener('DOMContentLoaded', function() {
             aba.classList.add('shows__list--is-active');
             removeBotaoAtivo();
             botao.target.classList.add('shows__tabs__button--is-active');
-            
         })
     }
 })
@@ -19,10 +17,9 @@ function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
 
     for (let i = 0; i < buttons.length; i++) {
-        buttons[i].classList.remove('shows__tabs__button--is-active');
+        buttons[i].classList.remove('shows__tabs__button--is-active')
     }
 }
-
 
 function escondeTodasAbas() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
@@ -30,5 +27,4 @@ function escondeTodasAbas() {
     for (let i = 0; i < tabsContainer.length; i++) {
         tabsContainer[i].classList.remove('shows__list--is-active');
     }
-
 }
